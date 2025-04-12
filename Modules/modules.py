f5890057852a82354f139138b3f6f1cc9ff1b874
borra-thirumala <thirumala2502@gmail.com>
2025-04-12 10:45:30 +0530

    changesmade

---
+++ Modules/modules.py
@@ -0,0 +1,54 @@
+# import math
+# print(math)    op:<module 'math' (built-in)>
+# This module provides access to the mathematical functions
+# defined by the C standard.
+
+
+
+# import math
+# print(math.ceil(1.2))  op:2    #Return the ceiling of x as an Integral.This is the smallest integer >= x.
+# print(math.floor(3.9))   op:3
+
+
+
+# import math
+# print(math.factorial(5))   op:120
+
+
+
+# import math
+# print(math.pow(5,2))       op:25.0
+
+
+# import math
+# print(math.sqrt(625))       op:25.0
+
+
+
+# import math
+# print(math.e)    #op:2.718281828459045
+
+
+
+# import math
+# print(math.pi)        #op:3.141592653589793
+
+
+
+#Perfect Square of a number
+
+# import math
+# num=int(input("enter the number: "))
+# if math.sqrt(num) < math.ceil(math.sqrt(num)):  #4.0 < 4
+#     print("not perfect")
+# else:
+#     print("perfect square")
+
+
+
+
+# import time
+# print(time)   #<module 'time' (built-in)>
+
+# import time
+# print(time.time())   op:1744428053.7162979 in milliseconds
